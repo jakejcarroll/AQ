ded019a2186a0addbace46c9fa5fa67b47a86962
jakejcarroll <57533010+jakejcarroll@users.noreply.github.com>
2019-12-08 15:29:29 +0000

    Create bme280.py

---
+++ bme280.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import time
+from bme280 import BME280
+
+try:
+    from smbus2 import SMBus
+except ImportError:
+    from smbus import SMBus
+
+bus = SMBus(1)
+bme280 = BME280(i2c_dev=bus)
+
+while True:
+    temperature = bme280.get_temperature()
+    pressure = bme280.get_pressure()
+    humidity = bme280.get_humidity()
+    time.sleep(1)
