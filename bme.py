47ec2827d9b7fb7feb8cb9d32a757534597ef2e9
jakejcarroll <57533010+jakejcarroll@users.noreply.github.com>
2019-12-08 19:52:28 +0000

    Update bme.py

--- bme.py
+++ bme.py
@@ -11,8 +11,6 @@ except ImportError:
 bus = SMBus(1)
 bme280 = BME280(i2c_dev=bus)
 
-while True:
-    temperature = bme280.get_temperature()
-    pressure = bme280.get_pressure()
-    humidity = bme280.get_humidity()
-    time.sleep(1)
+temperature = bme280.get_temperature()
+pressure = bme280.get_pressure()
+humidity = bme280.get_humidity()
