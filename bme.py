f0bb8590dd6e816aaaca312d9459f495d613da80
jakejcarroll <57533010+jakejcarroll@users.noreply.github.com>
2019-12-08 19:48:55 +0000

    Update bme.py

--- bme.py
+++ bme.py
@@ -8,19 +8,6 @@ try:
 except ImportError:
     from smbus import SMBus
 
-import logging
-
-logging.basicConfig(
-    format='%(asctime)s.%(msecs)03d %(levelname)-8s %(message)s',
-    level=logging.INFO,
-    datefmt='%Y-%m-%d %H:%M:%S')
-
-logging.info("""weather.py - Print readings from the BME280 weather sensor.
-
-Press Ctrl+C to exit!
-
-""")
-
 bus = SMBus(1)
 bme280 = BME280(i2c_dev=bus)
 
@@ -28,8 +15,4 @@ while True:
     temperature = bme280.get_temperature()
     pressure = bme280.get_pressure()
     humidity = bme280.get_humidity()
-    logging.info("""Temperature: {:05.2f} *C
-Pressure: {:05.2f} hPa
-Relative humidity: {:05.2f} %
-""".format(temperature, pressure, humidity))
     time.sleep(1)
