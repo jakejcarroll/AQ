18e1b596e20bec893244d47fe448220cb054b5f5
jakejcarroll <57533010+jakejcarroll@users.noreply.github.com>
2019-12-08 20:13:09 +0000

    Update pm.py

--- pm.py
+++ pm.py
@@ -5,7 +5,10 @@ from pms5003 import PMS5003, ReadTimeoutError
 import logging
 
 pms5003 = PMS5003()
+
 readings = pms5003.read()
 
-print(readings)
+pm1 = (readings.pm_ug_per_m3(1.0))
+
+print(pm1)
       
