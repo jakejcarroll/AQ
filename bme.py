a8b2da2001a7faae299bf43c8d7ddc330148210f
jakejcarroll <57533010+jakejcarroll@users.noreply.github.com>
2019-12-08 20:30:31 +0000

    Update bme.py

--- bme.py
+++ bme.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
 
-import time
-from bme280 import BME280
-
-try:
-    from smbus2 import SMBus
-except ImportError:
-    from smbus import SMBus
-
-bus = SMBus(1)
-bme280 = BME280(i2c_dev=bus)
-
-temperature = bme280.get_temperature()
-pressure = bme280.get_pressure()
-humidity = bme280.get_humidity()
+def bme():
+	import time
+	from bme280 import BME280
+	
+	try:
+	    from smbus2 import SMBus
+	except ImportError:
+	    from smbus import SMBus
+	
+	bus = SMBus(1)
+	bme280 = BME280(i2c_dev=bus)
+	
+	temperature = bme280.get_temperature()
+	pressure = bme280.get_pressure()
+	humidity = bme280.get_humidity()
