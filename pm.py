54ad4a6f1c8956181e8f1859699e761dc9580ab0
jakejcarroll <57533010+jakejcarroll@users.noreply.github.com>
2019-12-08 20:22:00 +0000

    Update pm.py

--- pm.py
+++ pm.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 
-import time
-from pms5003 import PMS5003, ReadTimeoutError
-import logging
-
-pms5003 = PMS5003()
-
-readings = pms5003.read()
-
-pm1 = (readings.pm_ug_per_m3(1.0))
-pm25 = (readings.pm_ug_per_m3(2.5))
-pm10 = (readings.pm_ug_per_m3(10))
+def pm():
+	import time
+	from pms5003 import PMS5003, ReadTimeoutError
+	import logging
+	
+	pms5003 = PMS5003()
+	
+	readings = pms5003.read()
+	
+	pm1 = (readings.pm_ug_per_m3(1.0))
+	pm25 = (readings.pm_ug_per_m3(2.5))
+	pm10 = (readings.pm_ug_per_m3(10))
       
