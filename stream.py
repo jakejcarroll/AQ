0dc7df10ac79c260976c50a3b108bee630913d08
jakejcarroll <57533010+jakejcarroll@users.noreply.github.com>
2019-12-09 10:39:33 +0000

    Update stream.py

--- stream.py
+++ stream.py
@@ -12,19 +12,32 @@ readings = pms5003.read()
 def pm1():
 	pm1_reading = (readings.pm_ug_per_m3(1.0))
 	return pm1_reading
+	pm1_reading.flush()
 
 def pm25():
 	pm25_reading = (readings.pm_ug_per_m3(2.5))
 	return pm25_reading
+	pm25_reading.flush()
 	
 def pm10():
 	pm10_reading = (readings.pm_ug_per_m3(10.0))
 	return pm10_reading
+	pm10_reading.flush()
 
-pm10()
 
 
 	
 	#input from calling function acts as request for specific pm(2.5/1/10)
 #e.g. pm25=pm(2.5)
 
+
+
+# main function
+
+def main():
+while True:
+	print(pm1)
+	print(pm25)
+	print(pm10)
+	time.sleep(5)
+	
