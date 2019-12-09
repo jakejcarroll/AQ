7dfd655c80e17e004e01d24fa9e5aa4cff580ecd
jakejcarroll <57533010+jakejcarroll@users.noreply.github.com>
2019-12-09 10:06:27 +0000

    Update pm.py

--- pm.py
+++ pm.py
@@ -10,6 +10,7 @@ readings = pms5003.read()
 
 def pm1():
 	pm1_reading = (readings.pm_ug_per_m3(1.0))
+	return pm1_reading
 	
 pm25 = (readings.pm_ug_per_m3(2.5))
 pm10 = (readings.pm_ug_per_m3(10))
